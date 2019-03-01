95e80cdf76f47a0bf55ca6ac691f2c8ef4a24e66
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-03-01 18:17:21 -0300

    feito model de usuarios e vt

--- sif/models/Vt.js
+++ sif/models/Vt.js
@@ -1,2 +1,57 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+// Create Schema
+const VtSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    nome: {
+      type: String,
+      required: true
+    },
+    vtmigrada: {
+        type: Boolean,
+        required: true
+    },
+    hdsituacao: {
+        type: String,
+        required: true
+    },
+    hds: [{
+        baia: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        }
+    }],
+    hdhistorico: [{
+        date: {
+            type: Date.now()
+        },
+        baia: {
+            type: String,
+        },
+        situacao: {
+            type: String
+        }
+    }],
+    fitabackupsituacao: {
+        type: String,
+        required: true
+    },
+    unidadefitahistorico: [{
+        date: {
+            type: Date.now()
+        },
+        observacao: {
+            type: String
+        }
+    }]
+  });
+  
+  module.exports = Vt = mongoose.model('Vts', VtSchema);
