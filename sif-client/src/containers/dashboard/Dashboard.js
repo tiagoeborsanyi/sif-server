7d43a7bc23fadf018e76e6130f087a655b8c00fb
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-04-08 13:59:29 -0300

    add basic dashboard

---
+++ sif-client/src/containers/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from '../../../axios-order';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Dashboard</h2>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
