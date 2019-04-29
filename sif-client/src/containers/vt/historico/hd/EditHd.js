e0d4d6cd28e98af40ae755a84d4037b6b768eed8
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-04-29 12:03:59 -0300

    feito layout de historico de hd

--- sif-client/src/containers/vt/historico/hd/EditHd.js
+++ sif-client/src/containers/vt/historico/hd/EditHd.js
@@ -5,8 +5,34 @@ import classes from './EditHd.css';
 class EditHd extends Component {
     render () {
         return (
-            <div className={classes.container}>
-                <h2>Edit HD</h2>
+            <div>
+                <div className={classes.hd}>
+                    <div className={classes.titulo}><h1>Histórico de troca de HD's</h1></div>
+                    <form>
+                        <div>
+                            <label>Data</label>
+                            <input type="date" placeholder="Data da troca" />
+                        </div>
+                        <div>
+                        <label>Baia trocada</label>
+                        <select>
+                            <option value="1">Baia 1</option>
+                            <option value="2">Baia 2</option>
+                            <option value="3">Baia 3</option>
+                            <option value="4">Baia 4</option>
+                            <option value="5">Baia 5</option>
+                            <option value="6">Baia 6</option>
+                            <option value="7">Baia 7</option>
+                            <option value="8">Baia 8</option>
+                        </select>
+                        </div>
+                        <div>
+                            <label>Observação</label>
+                            <textarea></textarea>
+                        </div>
+                        <button onClick={this.submitHandler}>Cadastrar</button>
+                    </form>
+                </div>
             </div>
         );
     }
