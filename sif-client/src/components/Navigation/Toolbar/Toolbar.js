326b9f407789a0c943476a95284a626219b9611e
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-05-01 12:18:03 -0300

    mudado navbar para class pra receber props-redux e state

--- sif-client/src/components/Navigation/Toolbar/Toolbar.js
+++ sif-client/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
 import classes from './Toolbar.css';
 
-const toolbar = (props) => (
-    <div>
-    <header>
-        <nav className={classes.navbar}>
-            <ul className={classes.nav__items}>
-                <li className={classes.logo}><NavLink to="/">Sif-Server</NavLink></li>
-                <li className={classes.search}><input type="text" placeholder="Search" /></li>
-                <li className={classes.links}><NavLink to="/cadastro-vt">ADD+</NavLink></li>
-                <li className={classes.links+' '+classes.list_1}>
-                    <span className={classes.link__user}>Usuario</span>
-                    <ul>
-                        <li>
-                            <NavLink to="/perfil-user">Perfil</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/cadastro-user">Cadastrar</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/logout">Sair</NavLink>
+class Toolbar extends Component {
+    render () {
+        return (
+            <div>
+            <header>
+                <nav className={classes.navbar}>
+                    <ul className={classes.nav__items}>
+                        <li className={classes.logo}><NavLink to="/">Sif-Server</NavLink></li>
+                        <li className={classes.search}><input type="text" placeholder="Search" /></li>
+                        <li className={classes.links}><NavLink to="/cadastro-vt">ADD+</NavLink></li>
+                        <li className={classes.links+' '+classes.list_1}>
+                            <span className={classes.link__user}>Usuario</span>
+                            <ul>
+                                <li>
+                                    <NavLink to="/perfil-user">Perfil</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/cadastro-user">Cadastrar</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/logout">Sair</NavLink>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    </div>
-);
+                </nav>
+            </header>
+            </div>
+        );
+    }
+}
 
-export default toolbar;
+export default Toolbar;
