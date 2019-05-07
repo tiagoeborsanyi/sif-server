2d35684c67d3ad00569e346bed3ccb77177d180d
Tiago Emerick <tiagoeborsanyi@gmail.com>
2019-05-07 00:00:07 -0300

    layout para mudar senha do usuario esta pronto

--- sif-client/src/containers/auth/editSenha/EditSenha.js
+++ sif-client/src/containers/auth/editSenha/EditSenha.js
@@ -5,11 +5,44 @@ import { connect } from 'react-redux';
 import classes from './EditSenha.css';
 
 class EditSenha extends Component {
+
+    state = {
+        cadastro: {
+            pass1: '',
+            pass2: ''
+        }
+    }
+
+    setChangeValue = (event) => {
+        const updateCadastro = {
+            ...this.state.cadastro
+        }
+        updateCadastro[event.target.name] = event.target.value;
+        this.setState({cadastro: updateCadastro});
+        
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+
     render () {
-        console.log(this.props.userId);
         return (
-            <div className={classes.container}>
-                <h3>Editar a senha do Usuario</h3>
+            <div>
+                <div className={classes.login}>
+                    <div className={classes.titulo}><h1>Mudar sua senha</h1></div>
+                    <form>
+                        <div>
+                            <label>Nova senha</label>
+                            <input onChange={this.setChangeValue} value={this.state.cadastro.pass1} name="pass1" type="password" placeholder="senha" />
+                        </div>
+                        <div>
+                            <label>Repita a Senha</label>
+                            <input onChange={this.setChangeValue} value={this.state.cadastro.pass2} name="pass2" type="password" placeholder="senha" />
+                        </div>
+                        <button onClick={this.submitHandler}>Cadastrar</button>
+                    </form>
+                </div>
             </div>
         );
     }
